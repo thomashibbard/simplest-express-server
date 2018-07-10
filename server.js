c6301634f34466c972776cd8fcd11cf68155c5f9
thibbard <thomas.hibbard@networkedinsights.com>
2018-07-10 11:15:42 -0500

    adding connection info to routes

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-const express = require('express'),
-  path = require('path'),
-  app = express()
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const cj = require('circular-json')
+const cors = require('cors')
 
-const PORT = 8081
+const app = express()
+const router = express.Router()
+const server = http.createServer(app)
+const port = 8081
 
+server.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
+app.use(cors())
 app.use('/static', express.static(path.resolve(__dirname, 'static')))
 
-app.get('*', (req, res, next) => {
-  console.log('loaded /*')
+app.get('/', (request, response, next) => {
+  response.sendFile(path.resolve(__dirname, 'index.html'))
   next()
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
-})
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT} ... 🤔`)
+app.get('/info', (request, response, next) => {
+  const info = cj.stringify(
+    {
+      serverAddress: server.address(),
+      routerStack: router.stack,
+      responseConnection: response.connection,
+      requestConnection: request.connection
+    },
+    null,
+    2
+  )
+  response.send(`<pre><code>${info}</code></pre>`)
 })
